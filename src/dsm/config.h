8f5909c831b5f59ffbb45e1645c64657997e89e0
Marcel Meek <marcel.meek@gmail.com>
2020-09-26 15:46:23 +0200

    Version 1.1  (use HW Serial with #undef SW_SERIAL)

--- src/dsm/config.h
+++ src/dsm/config.h
@@ -11,14 +11,17 @@
 #define _CONFIG_h
 
 // specify here TTN keys
-#define APPEUI "70B3D57ED0031D76"
-#define DEVEUI "0000000000000001"
-#define APPKEY "5D80E9639C45212F05F1230D4DCBC259"
+#define APPEUI "0000000000000000"
+#define DEVEUI "0000000000000000"
+#define APPKEY "00000000000000000000000000000000"
 
 // define in milleseconds how often a message will be sent
 #define CYCLETIME 600000  //10 minutes
 
-#define BAUDRATE 9600
-#define VERSION "1.0"
 
+// You must use Hardware Serial if baudrate > 57.6K
+// use HW serial
+#undef SW_SERIAL
+#define BAUDRATE 115200
+#define VERSION "1.1"
 #endif
